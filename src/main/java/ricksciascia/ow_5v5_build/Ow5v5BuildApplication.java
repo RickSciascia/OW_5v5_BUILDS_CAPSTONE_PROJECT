f1717c4d6978bdc38ecea1634c011232cc01f1e4
Riccardo <richi-97@live.it>
2026-03-03 10:47:09 +0100

    setup-commit

---
+++ src/main/java/ricksciascia/ow_5v5_build/Ow5v5BuildApplication.java
@@ -0,0 +1,13 @@
+package ricksciascia.ow_5v5_build;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ow5v5BuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ow5v5BuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ricksciascia/ow_5v5_build/Ow5v5BuildApplicationTests.java
@@ -0,0 +1,13 @@
+package ricksciascia.ow_5v5_build;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ow5v5BuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
