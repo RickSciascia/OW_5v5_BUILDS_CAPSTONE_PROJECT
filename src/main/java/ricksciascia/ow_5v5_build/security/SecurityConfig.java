02c7aa55ec3de9ccc7d1ca540ff0c81fc01ce191
Riccardo <richi-97@live.it>
2026-03-11 17:07:43 +0100

    added CorsConfig

--- src/main/java/ricksciascia/ow_5v5_build/security/SecurityConfig.java
+++ src/main/java/ricksciascia/ow_5v5_build/security/SecurityConfig.java
@@ -2,10 +2,16 @@ package ricksciascia.ow_5v5_build.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -17,8 +23,20 @@ public class SecurityConfig {
         httpSecurity.csrf(csrf->csrf.disable());
         httpSecurity.sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         httpSecurity.authorizeHttpRequests(r->r.requestMatchers("/**").permitAll());
-
+        httpSecurity.cors(Customizer.withDefaults());
 
         return httpSecurity.build();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
 }
