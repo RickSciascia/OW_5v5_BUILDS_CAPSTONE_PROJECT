31887127c7a446d73510fe4de7395d94b82b6007
Riccardo <richi-97@live.it>
2026-03-22 15:01:26 +0100

    updated CORS config to support PATCH method

--- src/main/java/ricksciascia/ow_5v5_build/dto/UserResponseDTO.java
+++ src/main/java/ricksciascia/ow_5v5_build/dto/UserResponseDTO.java
@@ -1,4 +1,8 @@
 package ricksciascia.ow_5v5_build.dto;
 
-public record UserResponseDTO(Long id, String username, String email, String role, String image) {
+public record UserResponseDTO(Long id,
+                              String username,
+                              String email,
+                              String role,
+                              String image) {
 }
